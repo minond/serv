821778cb81a1bce8f24f2cf953b2c82e2dc4f736
Marcos Minond <minond.marcos@gmail.com>
2017-11-26 18:20:45 -0700

    Revert "multiple domains"
    This reverts commit cbcd9d280352e19b63da10c2b39f6b74399f002f.

--- main.go
+++ main.go
@@ -245,8 +245,6 @@ func SetDirHandler(mux *http.ServeMux, route Route) {
 			filePath = indexFile
 		}
 
-		log.Printf("%s\n", r.Host)
-
 		loc := GuessFileInDir(filePath, route.Data)
 		log.Printf("serving %v from %v\n", r.URL.String(), loc)
 		http.ServeFile(w, r, loc)
@@ -281,10 +279,6 @@ func GuessFileInDir(file, dir string) string {
 	} else {
 		return origPath
 	}
-}
-
-func GetCleanHost(host string) string {
-	return strings.Split(host, ":")[0]
 }
 
 func main() {
