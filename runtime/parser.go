81b5bff31f0964636805dc167eb050b7047efb68
Marcos Minond <minond.marcos@gmail.com>
2018-01-10 16:25:16 -0700

    sample ast

--- runtime/parser.go
+++ runtime/parser.go
@@ -15,7 +15,7 @@ package runtime
  *                     | IDENTIFIER "(" [IDENTIFIER]* ")" ;
  *
  *
- * Sample:
+ * Sample raw input:
  *
  *     case Host(_, _, _) =>
  *       path /        := git(https://github.com/minond/minond.github.io.git)
@@ -26,69 +26,71 @@ package runtime
  *       path /imdb    := proxy(http://www.imdb.com:80)
  *       path /unibrow := proxy(http://localhost:3001)
  *
+ *
+ * Sample ast output:
+ *
+ *     var ast = []Case{
+ *       Case{
+ *     	   expr: Expr{
+ *           kind:  call,
+ *           value: Token{kind: caseToken, lexeme: "Host"},
+ *           args: []Token{
+ *             Token{kind: identifierToken, lexeme: "_"},
+ *             Token{kind: identifierToken, lexeme: "_"},
+ *             Token{kind: identifierToken, lexeme: "_"},
+ *           },
+ *         },
+ *         dcls: []Declaration{
+ *           Declaration{
+ *             kind: path,
+ *             key:  Token{kind: identifierToken, lexeme: "/"},
+ *             value: Expr{
+ *               kind:  call,
+ *               value: Token{kind: identifierToken, lexeme: "git"},
+ *               args: []Token{
+ *                 Token{kind: identifierToken, lexeme: "https://github.com/minond/minond.github.io.git"},
+ *               },
+ *             },
+ *           },
+ *           Declaration{
+ *             kind: path,
+ *             key:  Token{kind: identifierToken, lexeme: "/servies"},
+ *             value: Expr{
+ *               kind:  call,
+ *               value: Token{kind: identifierToken, lexeme: "git"},
+ *               args: []Token{
+ *                 Token{kind: identifierToken, lexeme: "https://github.com/minond/servies.git"},
+ *               },
+ *             },
+ *           },
+ *           Declaration{
+ *             kind: path,
+ *             key:  Token{kind: identifierToken, lexeme: "/static"},
+ *             value: Expr{
+ *               kind:  call,
+ *               value: Token{kind: identifierToken, lexeme: "dir"},
+ *               args: []Token{
+ *                 Token{kind: identifierToken, lexeme: "."},
+ *               },
+ *             },
+ *           },
+ *           Declaration{
+ *             kind: path,
+ *             key:  Token{kind: identifierToken, lexeme: "/ps"},
+ *             value: Expr{
+ *               kind:  call,
+ *               value: Token{kind: identifierToken, lexeme: "cmd"},
+ *               args: []Token{
+ *                 Token{kind: identifierToken, lexeme: "ps"},
+ *                 Token{kind: identifierToken, lexeme: "aux"},
+ *               },
+ *             },
+ *           },
+ *         },
+ *       },
+ *     }
+ *
  */
-
-var ast = []Case{
-	Case{
-		expr: Expr{
-			kind:  call,
-			value: Token{kind: caseToken, lexeme: "Host"},
-			args: []Token{
-				Token{kind: identifierToken, lexeme: "_"},
-				Token{kind: identifierToken, lexeme: "_"},
-				Token{kind: identifierToken, lexeme: "_"},
-			},
-		},
-		dcls: []Declaration{
-			Declaration{
-				kind: path,
-				key:  Token{kind: identifierToken, lexeme: "/"},
-				value: Expr{
-					kind:  call,
-					value: Token{kind: identifierToken, lexeme: "git"},
-					args: []Token{
-						Token{kind: identifierToken, lexeme: "https://github.com/minond/minond.github.io.git"},
-					},
-				},
-			},
-			Declaration{
-				kind: path,
-				key:  Token{kind: identifierToken, lexeme: "/servies"},
-				value: Expr{
-					kind:  call,
-					value: Token{kind: identifierToken, lexeme: "git"},
-					args: []Token{
-						Token{kind: identifierToken, lexeme: "https://github.com/minond/servies.git"},
-					},
-				},
-			},
-			Declaration{
-				kind: path,
-				key:  Token{kind: identifierToken, lexeme: "/static"},
-				value: Expr{
-					kind:  call,
-					value: Token{kind: identifierToken, lexeme: "dir"},
-					args: []Token{
-						Token{kind: identifierToken, lexeme: "."},
-					},
-				},
-			},
-			Declaration{
-				kind: path,
-				key:  Token{kind: identifierToken, lexeme: "/ps"},
-				value: Expr{
-					kind:  call,
-					value: Token{kind: identifierToken, lexeme: "cmd"},
-					args: []Token{
-						Token{kind: identifierToken, lexeme: "ps"},
-						Token{kind: identifierToken, lexeme: "aux"},
-					},
-				},
-			},
-		},
-	},
-}
-
 func Parse(raw string) []Case {
-	return ast
+	return []Case{}
 }
